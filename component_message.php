ba9e92ec17a036cc5bbcaf2f01752129aa92dc63
Ki Anom Dharma Bekti <dharmabekti@gmail.com>
2018-10-24 11:04:36 +0700

    component_message

---
+++ component_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <title>Partical Components Excercise: Message</title>
+      <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+      <style type="text/css">
+        body { padding-top: 50px; }
+      </style>
+     
+</head>
+<body>
+    <div id="root" class="container">
+      <message title="Hello World" body="Lorem ipsum dolor sit amet."></message>
+      <message title="Hello Universe" body="Lorem ipsum dolor sit amet."></message>
+      
+    </div>
+
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -23,6 +23,35 @@ Vue.component('task', {
 });
 
 
+Vue.component('message', {
+	props: ['title', 'body'],
+	data(){
+		return {
+			isVisible: true
+		};
+	},
+
+	template: `
+		<article class="message" v-show="isVisible">
+	        <div class="message-header">
+	          <p>{{ title }}</p>
+	          <button class="delete" aria-label="delete" @click="hideMessage"></button>
+	        </div>
+	        <div class="message-body">
+	          {{ body }}
+	        </div>
+	    </article>
+	`,
+	// Dapat juga menggunakan 
+	// <button class="delete" aria-label="delete" @click="isVisible = false"></button>
+
+	methods: {
+		hideMessage(){
+			this.isVisible=false;
+		}
+	}
+});
+
 new Vue({
 	el: '#root'
 });
